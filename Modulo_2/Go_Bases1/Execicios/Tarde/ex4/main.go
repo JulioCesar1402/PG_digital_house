63f7c0e095b0907686f4e345265a6577215b453a
Julio Cesar Monteiro Alves <jumonteiro@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-05-19 18:08:09 -0300

    ex 4

--- Modulo_2/Go_Bases1/Execicios/Tarde/ex4/main.go
+++ Modulo_2/Go_Bases1/Execicios/Tarde/ex4/main.go
@@ -11,8 +11,5 @@ func main() {
 		"Benjamin": 20, "Manuel": 26,
 		"Brenda": 19, "Dario": 44, "Pedro": 30,
 	}
-	fmt.Printf("%s tem %d anos", employee, employees[employee])
-
-	// month := time.Month(12)
-	// fmt.Println(month)
+	fmt.Printf("%s tem %d anos\n", employee, employees[employee])
 }
